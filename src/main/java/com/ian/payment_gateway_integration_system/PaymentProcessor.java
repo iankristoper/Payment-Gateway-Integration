426bfe0fdfe4c25887d4a4154bd9810eb62af33b
iankristoper <iankristoper26@gmail.com>
2024-04-07 14:28:25 +0800

    UPDATE: interface class features

---
+++ src/main/java/com/ian/payment_gateway_integration_system/PaymentProcessor.java
@@ -0,0 +1,11 @@
+package com.ian.payment_gateway_integration_system;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+public interface PaymentProcessor 
+{
+    public void processPayment(double amount);
+}
